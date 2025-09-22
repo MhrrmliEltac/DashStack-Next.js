4fbadefb2b6753e7c0a7e0569e816036f87c7a69
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-09-22 14:25:51 +0400

    refactor: add SettingsType to FieldsType for improved type safety in SettingsPage

--- src/app/(preferences)/settings/page.tsx
+++ src/app/(preferences)/settings/page.tsx
@@ -1,10 +1,10 @@
 import TitleHeader from "@/components/dashboard-heading/TitleHeader";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import SettingsFormElement from "@/components/settings/SettingsFormElement";
-import { type FieldsType } from "@/lib/types/types";
+import { SettingsType, type FieldsType } from "@/lib/types/types";
 import React from "react";
 
-const fields: FieldsType[] = [
+const fields: FieldsType<SettingsType>[] = [
   { label: "Site Name", id: "site_name", type: "text" },
   { label: "Copy Right", id: "copy_right", type: "text" },
   { label: "SEO Title", id: "seo_title", type: "text" },
