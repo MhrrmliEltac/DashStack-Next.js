bae6fef627636677937d1236055818489daf9c77
MhrrmliEltac <meherremlieltac14@gmail.com>
2025-09-23 17:38:48 +0400

    feat: implement search functionality in OrderListPage and ProductStockPage
    - Added search input to OrderListPage and ProductStockPage for filtering items.
    - Updated state management to handle search queries and filter displayed data accordingly.
    - Enhanced user experience by providing instant feedback on search results.

---
+++ src/app/(main)/inbox/page.tsx
@@ -0,0 +1,13 @@
+import TitleHeader from "@/components/dashboard-heading/TitleHeader";
+import DashboardLayout from "@/components/layout/DashboardLayout";
+import React from "react";
+
+const InboxPage = () => {
+  return (
+    <DashboardLayout>
+      <TitleHeader title="Inbox" />
+    </DashboardLayout>
+  );
+};
+
+export default InboxPage;
